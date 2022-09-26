5d0c1bba6e643c27fbd7686936b690168d2b1ade
Gliaudelis Gintaras <gliaudelis.gintaras@gmail.com>
2022-09-26 23:52:04 +0300

    хорошо рисует фигуры

--- wk2/ex1.py
+++ wk2/ex1.py
@@ -4,22 +4,20 @@ import matplotlib.pyplot as plt
 pi = np.pi
 
 def lissague_data(a, b, delta):
-    t = np.linspace(0, 2*pi, 50000, endpoint=False)
+    t = np.linspace(0, 2*pi, 10000, endpoint=False)
     x = np.sin(a * t + delta)
     y = np.sin(b * t)
     points = np.transpose(np.array((x, y)))
-    points = np.unique(points, axis=0)
+    #points = np.unique(points, axis=0)
     return points
 
 def lissague_draw(ax, x, y):
-    ax.scatter(x, y, s=1)
+    ax.plot(x, y, "-")
 
 fig, (ax) = plt.subplots(1, 1)
 fig.suptitle('Lissague')
 
-points = lissague_data(9, 8, pi/4)
-
+points = lissague_data(5, 3, pi/6)
 lissague_draw(ax, points[:,0], points[:,1])
-
 
 plt.show()
